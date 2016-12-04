376e52b20a84bf9783dacd3c42a9c537ef03a841
Martin Morávek (moravek.martin) <moravek.martin@gmail.com>
2016-12-04 16:17:07 +0100

    fixed next game dungeon estimate

--- app/controllers/news.js
+++ app/controllers/news.js
@@ -1,13 +1,18 @@
 import Ember from 'ember';
 import nextChatFromDate from '../utils/next-chat-from-date';
 import moment from 'moment';
+import DS from 'ember-data';
 const {computed, inject: {service}} = Ember;
+const {PromiseObject} = DS;
+
 
 export default Ember.Controller.extend({
   youtube: service(),
 
   nextGameDungeon: computed(function() {
-    return this.get('youtube').getNextGameDungeon();
+    const promise = this.get('youtube').getNextGameDungeon();
+
+    return PromiseObject.create({promise});
   }),
 
   nextVideoChat: computed(() => nextChatFromDate(moment())),
